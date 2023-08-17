a4e7aefe8daa10c97c6f5b5b055d3ebbd8c47661
Ramyac24 <75427300+Ramyac24@users.noreply.github.com>
2023-08-17 11:53:04 +0530

    Update activity.py

--- models/activity.py
+++ models/activity.py
@@ -1,1 +1,7 @@
 
+class Activity:
+    def __init__(self, event_type, resource_id, details):
+        self.event_type = event_type
+        self.resource_id = resource_id
+        self.details = details
+
