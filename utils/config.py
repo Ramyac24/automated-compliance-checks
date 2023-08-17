584a5a43167022a3132100815a715f8664adbc5e
Ramyac24 <75427300+Ramyac24@users.noreply.github.com>
2023-08-17 11:53:48 +0530

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,1 +1,16 @@
+COS_CONFIG = {
+    'access_key': 'YOUR_ACCESS_KEY_ID',
+    'secret_key': 'YOUR_SECRET_ACCESS_KEY',
+    'endpoint_url': 'YOUR_COS_ENDPOINT'
+}
+
+ACTIVITY_TRACKER_CONFIG = {
+    'instance_id': 'YOUR_INSTANCE_ID',
+    'event_type': 'configuration_change',
+}
+
+EVENT_NOTIFIER_CONFIG = {
+    'instance_id': 'YOUR_INSTANCE_ID',
+    'event_type': 'event_notification',
+}
 
