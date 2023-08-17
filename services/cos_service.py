d99dcd8189cbc03730d2b4aa3e044de6a7102266
Ramyac24 <75427300+Ramyac24@users.noreply.github.com>
2023-08-17 11:51:14 +0530

    Update cos_service.py

--- services/cos_service.py
+++ services/cos_service.py
@@ -1,1 +1,15 @@
+import ibm_boto3
+
+class COSManager:
+    def __init__(self, access_key, secret_key, endpoint_url):
+        self.cos = ibm_boto3.client('s3',
+                                    aws_access_key_id=access_key,
+                                    aws_secret_access_key=secret_key,
+                                    endpoint_url=endpoint_url)
+
+    def update_bucket_permissions(self, bucket_name, new_permissions):
+        try:
+            self.cos.put_bucket_acl(Bucket=bucket_name, ACL=new_permissions)
+        except ibm_boto3.exceptions.S3UploadFailedError as e:
+            print("Error updating bucket permissions: {}".format(e))
 
